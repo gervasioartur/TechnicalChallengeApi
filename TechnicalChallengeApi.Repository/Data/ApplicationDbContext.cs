340668e0945eaa563ae9331f074f25138976eb9d
gervasioartur <gervasioartur@outlook.com.br>
2024-11-07 21:30:12 -0300

    feat: add DbContext configurations

---
+++ TechnicalChallengeApi.Repository/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnicalChallengeApi.Domain.Entity;
+
+namespace TechnicalChallengeApi.Repository.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base (options) { }
+
+        public DbSet<Product> products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new ProductConfiguration());
+        }
+    }
+}
